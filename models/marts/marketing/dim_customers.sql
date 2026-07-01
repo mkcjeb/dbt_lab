91149ba2e7125d2398d3097e0f8a5c3f5512fda4
M B <michelleb.retail@gmail.com>
2026-07-01 20:46:31 +0000

    Refactor dim_customers.sql file

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,8 +1,8 @@
 with customers as (
-    select * from {{ ref ('stg_jaffle_shop__customers')}}
+    select * from {{ ref ('stg_jaffle_shop__customers') }}
 ),
 orders as (
-    select * from {{ ref ('fct_orders')}}
+    select * from {{ ref ('fct_orders') }}
 ),
 customer_orders as (
     select
@@ -14,15 +14,15 @@ customer_orders as (
     from orders
     group by 1
 ),
- final as (
+final as (
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce (customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_value
+        customer_orders.lifetime_value,
+        coalesce (customer_orders.number_of_orders, 0) as number_of_orders
     from customers
     left join customer_orders on customers.customer_id = customer_orders.customer_id
 )
